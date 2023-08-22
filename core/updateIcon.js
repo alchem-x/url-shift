cc91fffc9c486917f8b23d3e6fa6635aeda71e07
cbdyzj <cbdyzj@jianzhao.org>
2023-08-22 13:27:12 +0800

    Amend

--- core/updateIcon.js
+++ core/updateIcon.js
@@ -1,5 +1,4 @@
 export default async function updateIcon(enabled) {
     // const iconPath = enabled ? '/icon-48.png' : '/icon-gray-48.png'
     // await chrome.action.setIcon({ path: { 48: iconPath } })
-    return enabled ? await chrome.action.enable() : await chrome.action.disable()
 }
